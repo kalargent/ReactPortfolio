b9afdd1c02b0d3adef657502a0d5e4eef33cff6f
Karen Largent <karenlargent@Karens-MacBook-Pro.local>
2019-10-15 16:43:14 -0400

    updated the card text

--- src/components/ProjectCard/index.js
+++ src/components/ProjectCard/index.js
@@ -9,7 +9,8 @@ function ProjectCard(props) {
       <div className="col s12 m7">
         <div className="card">
           <div className="card-content black-text">
-            <span className="card-title">{props.title}</span>
+            <span className="title">{props.title}</span>
+            <p className="description">This is some description.</p>
           </div>
 
           <div className="card-action">  
