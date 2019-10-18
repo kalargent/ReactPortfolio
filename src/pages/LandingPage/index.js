21d6f04bdfd9c9ff9fc3670a097517a5788731fd
Karen Largent <karenlargent@Karens-MacBook-Pro.local>
2019-10-18 16:36:54 -0400

    updated my portfolio with a disclaimer that it is under construction

--- src/pages/LandingPage/index.js
+++ src/pages/LandingPage/index.js
@@ -4,12 +4,16 @@ import './style.css';
 
 const LandingPage = (props) =>{
     return (
-        <div className="landing-page"> 
+        <div className="landing-page">
+            <div className="disclaimer">
+            The user experience for this site is currently under construction. Scroll down to check out my portfolio. <br></br>
+            <a href="https://kalargent.github.io/bootstrap-portfolio/index.html" target="blank">Click Here</a> to view my old site.
+            </div> 
             <div className="intro">
                 <h2>Hi, I'm Karen</h2> 
             </div>
             <div className="tagline">
-                <h5>Product Manager | Full Stack Dev | Something else </h5>
+                <h5>Product Manager | Full Stack Web Developer </h5>
             </div>
 
             <div className="social">
