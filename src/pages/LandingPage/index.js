ec90227e725c642f7075e72da488876b4c8efc03
Karen Largent <karenlargent@Karens-MacBook-Pro.local>
2019-10-20 14:35:04 -0400

    styling landing page and adding a new folder for resume

--- src/pages/LandingPage/index.js
+++ src/pages/LandingPage/index.js
@@ -5,10 +5,10 @@ import './style.css';
 const LandingPage = (props) =>{
     return (
         <div className="landing-page">
-            <div className="disclaimer">
+            {/* <div className="disclaimer">
             The user experience for this site is currently under construction. Scroll down to check out my portfolio. <br></br>
             <a href="https://kalargent.github.io/bootstrap-portfolio/index.html" target="blank">Click Here</a> to view my old site.
-            </div> 
+            </div>  */}
             <div className="intro">
                 <h2>Hi, I'm Karen</h2> 
             </div>
@@ -18,8 +18,8 @@ const LandingPage = (props) =>{
 
             <div className="social">
             <a href = "https://www.linkedin.com/in/karen-largent-ab01494/" rel = "noopener noreferrer" target="_blank"><i className="fab fa-linkedin fa-3x"></i></a> 
-            <a href = "https://github.com/kalargent" rel = "noopener noreferrer" target="_blank"><i className="fab fa-github-square fa-3x"></i></a> 
-            <i class="fas fa-file fa-3x"></i>
+            <a href = "https://github.com/kalargent" rel = "noopener noreferrer" target="_blank"><i class="fab fa-github-alt fa-3x"></i></a> 
+            <a href="../../assets/KAL Resume Full Stack Dev.pdf" target="_blank"><i className="fas fa-file fa-3x"></i></a>
             </div>
         </div>
     )
