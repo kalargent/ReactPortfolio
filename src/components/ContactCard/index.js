ee5f51f8a90e3cc0d49b9d521406176074e8f4bf
Karen Largent <karenlargent@Karens-MacBook-Pro.local>
2019-10-20 18:12:00 -0400

    maybe adding a contact card....

---
+++ src/components/ContactCard/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./style.css";
+
+function ContactCard(props) {
+    return (
+        <div>
+            
+        </div>
+    );
+  }
+  
+  export default ContactCard;
