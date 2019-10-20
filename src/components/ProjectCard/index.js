3e0b2f424efbe99642acf12ff7bdc2d623de1af3
Karen Largent <karenlargent@Karens-MacBook-Pro.local>
2019-10-20 16:02:14 -0400

    updated project cards

--- src/components/ProjectCard/index.js
+++ src/components/ProjectCard/index.js
@@ -9,17 +9,20 @@ function ProjectCard(props) {
       <div className="col s12 m7">
         <div className="card">
           <div className="card-content black-text">
-          <i class="fas fa-laptop-code fas-md" color="black"></i> 
+          {/* <i class="fas fa-laptop-code fas-md fa-2x" color="black"></i>  */}
           <br></br>
+            {/* <span>{props.logo}</span> */}
             <span className="title">{props.title}</span>
             <p className="description">{props.description}</p>
+            <a href={props.git} target="blank"> <i class="fab fa-github-square fa-md fa-2x"></i></a>
+           <a href={props.prod} target="blank"> <i class="fas fa-link fa-md fa-2x"></i></a>
           </div>
 
-          <div className="card-action">  
-           <a href={props.git} target="blank"> <i class="fab fa-github-square fa-md"></i></a>
-           <a href={props.prod} target="blank"> <i class="fas fa-link fa-md"></i></a>
-           <a href={props.video}> <i class="fas fa-video fa-md"></i></a>
-          </div>
+          {/* <div className="card-action">   */}
+           {/* <a href={props.git} target="blank"> <i class="fab fa-github-square fa-md fa-2x"></i></a> */}
+           {/* <a href={props.prod} target="blank"> <i class="fas fa-link fa-md fa-2x"></i></a> */}
+           {/* <a href={props.video}> <i class="fas fa-video fa-md"></i></a> */}
+          {/* </div> */}
         </div>
       </div>
     </div>
--- src/pages/PortfolioPage/index.js
+++ src/pages/PortfolioPage/index.js
@@ -1,41 +1,41 @@
-import React from "react"; 
-import projects from "../../projects.json"; 
-import ProjectCard from "../../components/ProjectCard"
+import React from "react";
+import projects from "../../projects.json";
+import ProjectCard from "../../components/ProjectCard";
 // import project from "../../components/Projects/projects"
 
-import './style.css'; 
+import "./style.css";
 
-const PortfolioPage = (props) =>{
+const PortfolioPage = props => {
+  return (
+    // <div>
+    //   {project.map((item, i) => (
+    //     <Project render={item.render} key={i} />
+    //   ))}
+    // </div>
 
-    return (
+    <div className="portfolio-page">
+      <div className="portfolio-title">
+        <h2>Portfolio</h2>
+      </div>
 
-      // <div>
-      //   {project.map((item, i) => (
-      //     <Project render={item.render} key={i} />
-      //   ))}
-      // </div>
       
-        <div className="portfolio-page"> 
-            <div className="portfolio-title">
-               <h2>Portfolio</h2> 
-            </div>
-            <div className="project-cards">
-            {props.projects.map(projects => (
-              <ProjectCard
-                id={projects.id}
-                key={projects.id}
-                title={projects.name}
-                image={projects.image}
-                git={projects.git}
-                prod={projects.prod}
-                video={projects.video}
-                description={projects.description}
-              />
-            ))}
-            </div>
-        </div>
+      <div className="project-cards">
+        {props.projects.map(projects => (
+          <ProjectCard
+            id={projects.id}
+            key={projects.id}
+            title={projects.name}
+            image={projects.image}
+            git={projects.git}
+            prod={projects.prod}
+            video={projects.video}
+            description={projects.description}
+            logo={projects.logo}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
 
-    )
-}
-
-export default PortfolioPage; 
+export default PortfolioPage;
