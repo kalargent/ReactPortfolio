9e0fc5058002863907c2fc3730d3983467dff312
kalargent <kalargent@gmail.com>
2019-11-12 00:01:23 -0500

    got images to display imperfectly

--- src/components/ProjectCard/index.js
+++ src/components/ProjectCard/index.js
@@ -14,7 +14,8 @@ function ProjectCard(props) {
             {/* <span>{props.logo}</span> */}
             <span className="title">{props.title}</span>
             <p className="description">{props.description}</p>
-            
+            <img src={props.image}></img>
+            {/* <img src>{props.image}</img> */}
             <a href={props.git} target="blank"> <i class="fab fa-github-square fa-md fa-2x"></i></a>
            <a href={props.prod} target="blank"> <i class="fas fa-link fa-md fa-2x"></i></a>
           </div>
