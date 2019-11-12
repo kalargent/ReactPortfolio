e804531a439dcf7a3d0fd64d58ddd6a4cd77a850
kalargent <kalargent@gmail.com>
2019-11-12 01:05:12 -0500

    OOPS shoulda commented out the contact form before i pushed to prod

--- src/App.js
+++ src/App.js
@@ -24,7 +24,7 @@ class App extends React.Component {
         <PortfolioPage 
           projects={this.state.projects}
         />
-        <Contact />
+        {/* <Contact /> */}
       </div>
     );
   }
