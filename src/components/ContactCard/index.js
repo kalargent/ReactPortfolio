31752c8ea6f20f056c65c4f75661da0486d522e6
kalargent <kalargent@gmail.com>
2019-11-12 00:58:17 -0500

    Added a border around the images on cards

--- src/components/ContactCard/index.js
+++ src/components/ContactCard/index.js
@@ -1,12 +1,27 @@
 import React from "react";
-import "./style.css";
+// import "./style.css";
 
 function ContactCard(props) {
     return (
-        <div>
-            
+        <div className="container">
+
+            <form id="contact-form" method="POST">
+                <div className="form-group">
+                    <label for="name">Name</label>
+                    <input type="text" className="form-control" id="name" />
+                </div>
+                <div className="form-group">
+                    <label for="exampleInputEmail1">Email address</label>
+                    <input type="email" className="form-control" id="email" aria-describedby="emailHelp" />
+                </div>
+                <div className="form-group">
+                    <label for="message">Message</label>
+                    <textarea className="form-control" rows="5" id="message"></textarea>
+                </div>
+                <button type="submit" className="btn btn-primary">Submit</button>
+            </form>
         </div>
     );
-  }
-  
-  export default ContactCard;
+}
+
+export default ContactCard;
--- src/pages/Contact/index.js
+++ src/pages/Contact/index.js
@@ -1,11 +1,13 @@
 import React from "react"; 
 import './style.css'; 
+import ContactCard from "../../components/ContactCard";
 
 const Contact = () => {
     return (
         <div className="contact-page"> 
             <div className="contact-title">
                 <h2>Contact</h2>
+                <ContactCard />
             </div>
             
         </div>
