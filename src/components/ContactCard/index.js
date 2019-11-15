6587b60e2ba21d0db5948a874de3e30345aa5782
Karen Largent <karenlargent@MacBook-Pro.local>
2019-11-15 15:42:02 -0500

    updated the contact form

--- src/components/ContactCard/index.js
+++ src/components/ContactCard/index.js
@@ -1,59 +1,75 @@
-import React, { Component } from 'react';
-import axios from 'axios';
+import React, { Component } from "react";
+import axios from "axios";
 
 class ContactForm extends Component {
+  // handleSubmit(e) {
+  //     console.log("submitted");
+  //     e.preventDefault();
+  //     const name = document.getElementById('name').value;
+  //     const email = document.getElementById('email').value;
+  //     const message = document.getElementById('message').value;
+  //     axios({
+  //         method: "POST",
+  //         url: "http://localhost:3002/send",
+  //         data: {
+  //             name: name,
+  //             email: email,
+  //             messsage: message
+  //         }
+  //     }).then((response) => {
+  //         if (response.data.msg === 'success') {
+  //             alert("Message Sent.");
+  //             this.resetForm()
+  //         } else if (response.data.msg === 'fail') {
+  //             alert("Message failed to send.")
+  //         }
+  //     })
+  // }
 
-    handleSubmit(e) {
-        console.log("submitted");
-        e.preventDefault();
-        const name = document.getElementById('name').value;
-        const email = document.getElementById('email').value;
-        const message = document.getElementById('message').value;
-        axios({
-            method: "POST",
-            url: "http://localhost:3002/send",
-            data: {
-                name: name,
-                email: email,
-                messsage: message
-            }
-        }).then((response) => {
-            if (response.data.msg === 'success') {
-                alert("Message Sent.");
-                this.resetForm()
-            } else if (response.data.msg === 'fail') {
-                alert("Message failed to send.")
-            }
-        })
-    }
+  // resetForm() {
+  //     document.getElementById('contact-form').reset();
+  // }
 
-    resetForm() {
-        document.getElementById('contact-form').reset();
-    }
+  render() {
+    return (
+      <div className="container">
 
-    render() {
-        return (
-            <div className="container">
-                <div className="col-sm-4 offset-sm-4">
-                    <form id="contact-form" onSubmit={this.handleSubmit.bind(this)} method="POST">
-                        <div className="form-group">
-                            <label for="name">Name</label>
-                            <input type="text" className="form-control" id="name" />
-                        </div>
-                        <div className="form-group">
-                            <label for="exampleInputEmail1">Email address</label>
-                            <input type="email" className="form-control" id="email" aria-describedby="emailHelp" />
-                        </div>
-                        <div className="form-group">
-                            <label for="message">Message</label>
-                            <textarea className="form-control" rows="5" id="message"></textarea>
-                        </div>
-                        <button type="submit" className="btn btn-primary">Submit</button>
-                    </form>
-                </div>
-            </div>
-        )
-    }
+        <form action="/contact" id="contact-form" method="post" role="form">
+            
+          <fieldset>
+            <label for="name">Name</label>
+            <input
+              id="name"
+              name="name"
+              type="text"
+              placeholder="Your name"
+              required
+            />
+
+            <label for="email">Email</label>
+            <input
+              id="email"
+              name="email"
+              type="text"
+              placeholder="Your email"
+              required
+            />
+
+            <label for="message">Message</label>
+            <textarea
+              id="message"
+              name="message"
+              placeholder="Enter your message here"
+              rows="3"
+              required
+            />
+
+            <button type="submit">Submit</button>
+          </fieldset>
+        </form>
+      </div>
+    );
+  }
 }
 
-export default ContactForm; 
+export default ContactForm;
